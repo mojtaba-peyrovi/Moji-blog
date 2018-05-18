7246a17ad9cff1ddd86325e3bf820e336ca9bd13
unknown <mojtaba.peyrovi@gmail.com>
2018-05-18 10:28:58 +0700

    end of part 27- Markdown Mail

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\User;
+use App\Mail\Welcome;
 
 class RegistrationController extends Controller
 {
@@ -22,6 +23,8 @@ class RegistrationController extends Controller
         ]);
 
         auth()->login($user);
+
+        \Mail::to($user)->send(new Welcome($user));
 
         return redirect()->route('index');
     }
